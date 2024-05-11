092e4ae2144adf9502df53ee7597cb85aa08d65a
KmiloLopez <andres.lopez2018@outlook.es>
2024-05-10 19:37:32 -0500

    Badge added and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,27 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { DateCalendar, DatePicker } from '@mui/x-date-pickers';
+import Badge from '@mui/material/Badge';
+import { PickersDay } from '@mui/x-date-pickers/PickersDay';
+import ShortTextIcon from '@mui/icons-material/ShortText';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import WysiwygIcon from '@mui/icons-material/Wysiwyg';
+
+import dayjs from 'dayjs' // ES 2015
+import CalendarMUI from './components/calendarMU/CalendarMUI';
+dayjs().format()
+
 
 const App = () => {
-  const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Vite + React</h1>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://react.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-};
-
+  const day= "day"
+	return (
+		<div className="contenedor">
+			<div className="grupo">
+				<CalendarMUI/>
+      		
+			</div>
+		</div>
+	);
+}
+ 
 export default App;
